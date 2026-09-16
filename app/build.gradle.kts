cc7e20f3fcc67ab39e142cb802d5cc54624a8ee7
Tommaso Vietina <tommasovietina@hotmail.com>
2026-09-16 15:48:51 +0200

    feat: Aggiornato versionCode e versionName a 24 e "1.7-beta1"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.tommasov.mg4simplelauncher"
         minSdk = 28
         targetSdk = 34
-        versionCode = 23
-        versionName = "1.7-test3"
+        versionCode = 24
+        versionName = "1.7-beta1"
 
         // Base URL of the update server; the version manifest lives at <base>/version.json
         buildConfigField(
