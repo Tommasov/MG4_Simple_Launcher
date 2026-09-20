f21dae14373a789ca7bcd28423edc99f41470d64
Tommaso Vietina <tommasovietina@hotmail.com>
2026-09-21 00:00:50 +0200

    Aggiornamento della versione a 1.8-beta1 e incremento del versionCode a 31 nel file build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.tommasov.mg4simplelauncher"
         minSdk = 28
         targetSdk = 34
-        versionCode = 30
-        versionName = "1.7"
+        versionCode = 31
+        versionName = "1.8-beta1"
 
         // Base URL of the update server; the version manifest lives at <base>/version.json
         buildConfigField(
